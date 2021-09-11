556a6fa0e9398036eab74da41f1bd0825aead731
uberfig <aidan.gifford04@gmail.com>
2021-09-11 16:08:49 -0400

    created build UI

--- addons/build-and-deploy/Build.gd
+++ addons/build-and-deploy/Build.gd
@@ -1,3 +1,45 @@
 tool
 extends Control
 
+
+var build_presets = {
+	"directory": "",
+	"game": "",
+	"channels": {
+		"Windows": false,
+		"Linux": false,
+		"Mac": false,
+		"HTML5": false
+	},
+	"async mode": true
+}
+
+func _ready():
+	print("executable path: ", OS.get_executable_path())
+
+func _on_BuildDialog_dir_selected(dir):
+	print("Selected directory: ", dir)
+	$VBoxContainer/BuildDir.text = dir
+
+
+func _on_SelectDir_pressed():
+	$FileDialog.popup()
+
+
+func _on_FileDialog_dir_selected(dir):
+	print("Selected directory: ", dir)
+	$VBoxContainer/BuildDir.text = dir
+
+
+func update_presets():
+	build_presets["directory"] = $VBoxContainer/BuildDir.text
+	build_presets["game"] = $VBoxContainer/Game.text
+	build_presets["channels"]["Windows"] = $VBoxContainer/Windows.is_pressed()
+	build_presets["channels"]["Linux"] = $VBoxContainer/Linux.is_pressed()
+	build_presets["channels"]["Mac"] = $VBoxContainer/Mac.is_pressed()
+	build_presets["channels"]["HTML5"] = $VBoxContainer/HTML5.is_pressed()
+	build_presets["async mode"] = $VBoxContainer/Async.is_pressed()
+	print("build presets updated with: ", build_presets)
+
+
+
--- addons/build-and-deploy/Dock Script.gd
+++ addons/build-and-deploy/Dock Script.gd
@@ -1,33 +1,40 @@
 tool
 extends Control
 
-
  
 var plugin_persistant_data = "res://addons/build-and-deploy/persistant.save"
 
 
-
-var blank_presets_dict: Dictionary = {
-	"is logged in": false,
-	"directory": "",
-	"user": "",
-	"game": "",
-	"channels": {
-		"Windows": false,
-		"Linux": false,
-		"Mac": false,
-		"HTML5": false
-	},
-	"async mode": true
+var persistant_dict = {
+	"Build": {},
+	"Deploy": {}
 }
+
+
+#var blank_presets_dict: Dictionary = {
+#	"is logged in": false,
+#	"directory": "",
+#	"user": "",
+#	"game": "",
+#	"channels": {
+#		"Windows": false,
+#		"Linux": false,
+#		"Mac": false,
+#		"HTML5": false
+#	},
+#	"async mode": true
+#}
 
 
 func save_build_presets():
 	$Deploy.update_presets()
+	$Build.update_presets()
 	print("saving deploy presets with: ", $Deploy.presets_dict)
+	persistant_dict["Deploy"] = $Deploy.presets_dict
+	persistant_dict["Build"] = $Build.build_presets
 	var file = File.new()
 	file.open(plugin_persistant_data, File.WRITE)
-	file.store_var($Deploy.presets_dict, true)
+	file.store_var(persistant_dict, true)
 	file.close()
 
 
@@ -35,13 +42,16 @@ func load_build_presets():
 	var file = File.new()
 	if file.file_exists(plugin_persistant_data):
 		file.open(plugin_persistant_data, File.READ)
-		$Deploy.presets_dict = Dictionary(file.get_var())
+		persistant_dict = Dictionary(file.get_var())
+		$Build.build_presets = persistant_dict["Build"]
+		$Deploy.presets_dict = persistant_dict["Deploy"]
 		file.close()
 		update_ui()
 	else:
-		
+		persistant_dict["Build"] = $Build.build_presets
+		persistant_dict["Deploy"] = $Deploy.presets_dict
 		file.open(plugin_persistant_data, File.WRITE)
-		file.store_var($Deploy.presets_dict, true)
+		file.store_var(persistant_dict, true)
 		file.close()
 
 
@@ -56,6 +66,16 @@ func update_ui():
 	$Deploy/VBoxContainer/Mac.set_pressed(deploy_presets["channels"]["Mac"])
 	$Deploy/VBoxContainer/HTML5.set_pressed(deploy_presets["channels"]["HTML5"])
 	$Deploy/VBoxContainer/Async.set_pressed(deploy_presets["async mode"])
+	
+	var Build_presets = $Build.build_presets
+	print("updating ui with: ", Build_presets)
+	$Build/VBoxContainer/Game.text = Build_presets["game"]
+	$Build/VBoxContainer/BuildDir.text = Build_presets["directory"]
+	$Build/VBoxContainer/Windows.set_pressed(Build_presets["channels"]["Windows"])
+	$Build/VBoxContainer/Linux.set_pressed(Build_presets["channels"]["Linux"])
+	$Build/VBoxContainer/Mac.set_pressed(Build_presets["channels"]["Mac"])
+	$Build/VBoxContainer/HTML5.set_pressed(Build_presets["channels"]["HTML5"])
+	$Build/VBoxContainer/Async.set_pressed(Build_presets["async mode"])
 
 
 func _ready():
