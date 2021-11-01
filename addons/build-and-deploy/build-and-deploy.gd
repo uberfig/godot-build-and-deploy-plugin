a8794734b6b1ade95016ecd973ac9cf8f9f90f12
uberfig <71034402+uberfig@users.noreply.github.com>
2021-11-01 13:45:38 -0400

    begining removal of the build menu

--- addons/build-and-deploy/build-and-deploy.gd
+++ addons/build-and-deploy/build-and-deploy.gd
@@ -9,7 +9,7 @@ var dock
 func _enter_tree():
 	# Initialization of the plugin goes here.
 	# Load the dock scene and instance it.
-	dock = preload("res://addons/build-and-deploy/Build and Deploy.tscn").instance()
+	dock = preload("res://addons/build-and-deploy/Build_and_Deploy.tscn").instance()
 
 	# Add the loaded scene to the docks.
 	add_control_to_dock(DOCK_SLOT_RIGHT_UL, dock)
